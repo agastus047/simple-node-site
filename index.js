8249094529e95d2d0067a4a9106f566e6247e524
Agastus Joyson <agastusjoyson@gmail.com>
2023-05-29 15:48:21 +0530

    Updates

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 const http = require('http');
+const fs = require('fs/promises');
+const path = require('path');
 
 const hostname = 'localhost';
 const port = 8080;
 
-const server = http.createServer((req, res) => {
+async function readContentOfFile(url) {
+    try{
+        let filePath;
+        switch(url) {
+            case '/':
+                filePath = path.join(__dirname,'index.html');
+                break;
+            case '/about':
+                filePath = path.join(__dirname,'about.html');
+                break;
+            case '/contact':
+                filePath = path.join(__dirname,'contact.html');
+                break;
+            default:
+                filePath = path.join(__dirname,'404.html');
+        }
+        const fileContent = await fs.readFile(filePath,'utf8');
+        return fileContent;
+    }
+    catch(err) {
+        console.log(err);
+    }
+    
+}
+
+const server = http.createServer(async(req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+    res.setHeader('Content-Type', 'text/html');
+    const fileContent = await readContentOfFile(req.url);
+    res.end(fileContent);
 });
   
 server.listen(port, hostname, () => {
