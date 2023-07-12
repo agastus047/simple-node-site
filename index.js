2f229d4b847ff3a264b22d34ed63d43bfd1c8ded
Agastus Joyson <agastusjoyson@gmail.com>
2023-07-12 17:24:31 +0530

    Use express

--- index.js
+++ index.js
@@ -1,42 +1,25 @@
-const http = require('http');
-const fs = require('fs/promises');
+const express = require('express');
 const path = require('path');
+const app = express();
 
-const hostname = 'localhost';
 const port = 8080;
 
-async function readContentOfFile(url) {
-    try{
-        let filePath;
-        switch(url) {
-            case '/':
-                filePath = path.join(__dirname,'index.html');
-                break;
-            case '/about':
-                filePath = path.join(__dirname,'about.html');
-                break;
-            case '/contact':
-                filePath = path.join(__dirname,'contact.html');
-                break;
-            default:
-                filePath = path.join(__dirname,'404.html');
-        }
-        const fileContent = await fs.readFile(filePath,'utf8');
-        return fileContent;
-    }
-    catch(err) {
-        console.log(err);
-    }
-    
-}
-
-const server = http.createServer(async(req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    const fileContent = await readContentOfFile(req.url);
-    res.end(fileContent);
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname,'index.html'));
 });
-  
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname,'about.html'));
+});
+
+app.get("/contact", (req, res) => {
+    res.sendFile(path.join(__dirname,'contact.html'));
+});
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname,'404.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
 });
